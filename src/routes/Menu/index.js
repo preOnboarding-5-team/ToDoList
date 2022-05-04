ac7df555f5be700669b85b670cebba3f0653aa1b
Jaehyuk <jaehyukj3@gmail.com>
2022-05-04 16:52:09 +0900

    Menu/index.js, Menu/Menu.module.scss 생성

---
+++ src/routes/Menu/index.js
@@ -0,0 +1,11 @@
+import styles from './Menu.module.scss';
+
+function Menu() {
+  return (
+    <div className={styles.menu}>
+      <div className={styles.centering} />
+    </div>
+  );
+}
+
+export default Menu;
