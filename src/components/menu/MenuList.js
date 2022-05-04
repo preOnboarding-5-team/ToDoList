bc7e9647cd2bded492ed3fdae17fe5faf38a9e2d
Jaehyuk <jaehyukj3@gmail.com>
2022-05-04 17:04:29 +0900

    MenuList 문자 출력 부분 작성 중

--- src/components/menu/MenuList.js
+++ src/components/menu/MenuList.js
@@ -1,5 +1,22 @@
 import styles from '../../routes/Menu/Menu.module.scss';
 
-function MenuList() {}
+function MenuList() {
+  return (
+    <ul className={styles.menulist}>
+      <li key="tamplates" className={styles.userMenu}>
+        <p className={styles.menusTitle}>Tamplates</p>
+      </li>
+      <li key="categories" className={styles.userMenu}>
+        <p className={styles.menusTitle}>Categories</p>
+      </li>
+      <li key="analystics" className={styles.userMenu}>
+        <p className={styles.menusTitle}>Analytics</p>
+      </li>
+      <li key="settings" className={styles.userMenu}>
+        <p className={styles.menusTitle}>Settings</p>
+      </li>
+    </ul>
+  );
+}
 
 export default MenuList;
