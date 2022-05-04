2389310755c45b182f4d30ec178182ab79f1077d
Jaehyuk <jaehyukj3@gmail.com>
2022-05-04 17:01:35 +0900

    components 디렉토리 생성, components 디렉토리 하위에 UserProfile.js, MenuList.js, CloseMenuButton.js 생성, UserProfile 일부 작성

---
+++ src/components/menu/UserProfile.js
@@ -0,0 +1,19 @@
+import styles from '../../routes/Menu/Menu.module.scss';
+
+function UserProfile() {
+  return (
+    <>
+      <div className={styles.userImageWrapper}>
+        <img
+          className={styles.userImage}
+          alt="test"
+          src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_960_720.jpg"
+        />
+      </div>
+      <h1>Joy</h1>
+      <h1>Mitchell</h1>
+    </>
+  );
+}
+
+export default UserProfile;
