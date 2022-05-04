67d90006349e993eb8e054db3a40d6955e9d85c7
Jaehyuk <jaehyukj3@gmail.com>
2022-05-04 17:04:29 +0900

    MenuList 문자 출력 부분 작성 중

--- src/routes/Menu/index.js
+++ src/routes/Menu/index.js
@@ -1,11 +1,13 @@
 import styles from './Menu.module.scss';
 import UserProfile from '../../components/menu/UserProfile';
+import MenuList from '../../components/menu/MenuList';
 
 function Menu() {
   return (
     <div className={styles.menu}>
       <div className={styles.centering} />
       <UserProfile />
+      <MenuList />
     </div>
   );
 }
