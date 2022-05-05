b1a40e0c227e51fa6e7390dd6ee9c751cce20e5b
Jaehyuk <jaehyukj3@gmail.com>
2022-05-05 13:53:27 +0900

    오타 수정, closemenubutton, menulist, userprofile 분리

--- src/components/menu/CloseMenuButton.js
+++
@@ -1,7 +0,0 @@
-import styles from '../../routes/Menu/Menu.module.scss';
-
-function CloseMenuButton() {
-  return <button type="button" className={styles.closeMenuButton} aria-label="Close menu button" />;
-}
-
-export default CloseMenuButton;
