aaa21a23278424b26aa4ad4686e5fcbb662cfb14
MGanom <chewy111@naver.com>
2022-05-06 06:13:38 +0900

    added: Category component;

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Category.module.scss';
 
-const Category = ({ array }) => {
-  const length = array.length;
-  const percentage = 100 / length;
+function Category({ categories, todoList }) {
+  const [categoryList, setCategoryList] = useState({});
+  const containerRef = useRef();
+
+  let position = { left: 0, x: 0 };
+  const onMouseMove = (e) => {
+    const x = e.clientX - position.x;
+
+    containerRef.current.scrollLeft = position.left - x;
+  };
+
+  const onMouseUp = () => {
+    containerRef.current.style.cursor = 'grab';
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  const onMouseDown = (e) => {
+    position = {
+      left: containerRef.current.scrollLeft,
+      x: e.clientX,
+    };
+    containerRef.current.style.cursor = 'grabbing';
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  useEffect(() => {
+    categories.forEach((category) => {
+      const card = todoList.filter((todo) => todo.category === category);
+      setCategoryList((prev) => ({ ...prev, [category]: card }));
+    });
+  }, [categories, todoList]);
+
   return (
-    <div>
-      <div className="sliderContainer">
-        <span className="sliderBar" />
-      </div>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div ref={containerRef} className={styles.container} onMouseDown={onMouseDown}>
+      {Object.keys(categoryList).map((category, idx) => {
+        const key = `card-key-${idx}`;
+        const total = categoryList[category].length;
+        const done = categoryList[category].filter((todo) => todo.done).length;
+        const doneRatio = (100 / total) * done;
+        return (
+          <div key={key} className={styles.card}>
+            <div className={styles.total}>{total} tasks</div>
+            <div className={styles.title}>{category}</div>
+            <div className={styles.progress}>
+              <span
+                className={styles.progressBar}
+                style={{
+                  width: `${doneRatio}%`,
+                }}
+              />
+            </div>
+            <div className={styles.percentage}>{Math.floor(doneRatio)}%</div>
+          </div>
+        );
+      })}
     </div>
   );
-};
+}
 
 export default Category;
--- src/routes/TodoList/index_original.js
+++ src/routes/TodoList/index_original.js
@@ -1,62 +1,27 @@
 import { useState } from 'react';
 import styles from './TodoList.module.scss';
 import { CheckIcon } from '../../assets/svgs';
-import Category from '../../components/category/Category';
 
 const INIT_TODO = [
   {
     id: 1,
     title: '계란 2판 사기',
-    category: '쇼핑',
     done: false,
   },
   {
     id: 2,
     title: '맥북 프로 M1 Max CTO 버전 사기',
-    category: '쇼핑',
     done: false,
   },
   {
     id: 3,
     title: '오늘의 TIL 작성하기',
-    category: '공부',
-    done: false,
-  },
-  {
-    id: 4,
-    title: '숨쉬기',
-    category: '운동',
-    done: false,
-  },
-  {
-    id: 5,
-    title: '우유사기',
-    category: '쇼핑',
-    done: false,
-  },
-  {
-    id: 6,
-    title: '러닝머신 뛰기',
-    category: '운동',
-    done: false,
-  },
-  {
-    id: 7,
-    title: '풀업하기',
-    category: '운동',
-    done: false,
-  },
-  {
-    id: 8,
-    title: '벤치프레스하기',
-    category: '운동',
     done: false,
   },
 ];
-const CATEGORIES = ['쇼핑', '공부', '운동'];
+
 function TodoList() {
   const [todoList, setTodoList] = useState(INIT_TODO);
-  const [categories, setCategories] = useState(CATEGORIES);
 
   const handleAddClick = (e) => {
     // console.log('handleAddClick')
@@ -79,7 +44,6 @@ function TodoList() {
       <input className="button" />
       <div className={styles.centering}>
         <h1>Hi! this is your assignment.</h1>
-        <Category categories={categories} todoList={todoList} />
         <ul className={styles.tasks}>
           <p className={styles.tasksTitle}>Today&apos;s</p>
           {todoList.map((todo) => (
