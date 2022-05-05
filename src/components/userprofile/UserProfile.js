69e0aface24fa56f3a1a3b01ade54f589280754d
Jaehyuk <jaehyukj3@gmail.com>
2022-05-05 16:11:23 +0900

    에러 수정 userprofile, menulist, closemenubutton 디자인 수정

--- src/components/userprofile/UserProfile.js
+++ src/components/userprofile/UserProfile.js
@@ -6,7 +6,7 @@ function UserProfile() {
       <div className={styles.userImageWrapper}>
         <img
           className={styles.userImage}
-          alt="test"
+          alt="test_userimage"
           src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_960_720.jpg"
         />
       </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ import Menu from './Menu';
 function App() {
   return (
     <div className={styles.app}>
-      <TodoList />
       <Menu />
+      <TodoList />
     </div>
   );
 }
