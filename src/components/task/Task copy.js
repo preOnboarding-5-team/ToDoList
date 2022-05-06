03f20531ab2f9de96247515695ce76219d80b989
restareaByWeezy <64529155+restareaByWeezy@users.noreply.github.com>
2022-05-07 01:42:18 +0900

    pull

---
+++ src/components/task/Task copy.js
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import styles from '../../routes/TodoList/TodoList.module.scss';
+import { CheckIcon } from '../../assets/svgs';
+
+const INIT_TODO = [
+  {
+    id: 1,
+    title: '계란 2판 사기',
+    done: false,
+  },
+  {
+    id: 2,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+  },
+  {
+    id: 3,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+  },
+  {
+    id: 4,
+    title: '계란 2판 사기',
+    done: false,
+  },
+  {
+    id: 5,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+  },
+  {
+    id: 6,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+  },
+  {
+    id: 7,
+    title: '계란 2판 사기',
+    done: false,
+  },
+  {
+    id: 8,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+  },
+  {
+    id: 9,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+  },
+];
+
+function Task() {
+  const [todoList, setTodoList] = useState(INIT_TODO);
+  // const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    const target = e.currentTarget;
+    const { dataset, checked } = target;
+    const { id } = dataset;
+
+    setTodoList((prev) => {
+      const targetIndex = prev.findIndex((todo) => todo.id === Number(id));
+      const newList = [...prev];
+      newList[targetIndex].done = checked;
+      // setIsChecked((prevIsChecked) => !prevIsChecked);
+      return newList;
+    });
+  };
+
+  return (
+    <>
+      <h1>Hi! this is your assignment.</h1>
+      <p className={styles.tasksTitle}>Today&apos;s</p>
+      <ul className={styles.tasks}>
+        {todoList.map((todo) => (
+          <li key={`todo-${todo.id}`} className={styles.task}>
+            <div className={styles.checkboxWrapper}>
+              <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={handleChange} />
+              <CheckIcon />
+            </div>
+            <p className={styles.title}>{todo.title}</p>
+            {/* <p className={`${styles.title} + ${!isChecked ? '' : styles.clear}`}>{todo.title}</p> */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Task;
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,68 +1,182 @@
 import { useState } from 'react';
 import styles from '../../routes/TodoList/TodoList.module.scss';
 import { CheckIcon } from '../../assets/svgs';
-import { INIT_TODO } from './initialData';
-import Swiper from './Swiper';
-import { LeadingActions, SwipeableList, SwipeableListItem, SwipeAction, TrailingActions } from 'react-swipeable-list';
-import 'react-swipeable-list/dist/styles.css';
+import DeleteIcon from '../../assets/pngs/delete.png';
+
+const INIT_TODO = [
+  {
+    id: 1,
+    title: '계란 2판 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 2,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 3,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 4,
+    title: '계란 2판 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 5,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 6,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 7,
+    title: '계란 2판 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 8,
+    title: '맥북 프로 M1 Max CTO 버전 사기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+  {
+    id: 9,
+    title: '오늘의 TIL 작성하기',
+    done: false,
+    category: 'personal',
+    undo: false,
+    deleted: false,
+  },
+];
 
 function Task() {
   const [todoList, setTodoList] = useState(INIT_TODO);
-
-  const trailingActions = () => (
-    <TrailingActions>
-      <SwipeAction destructive>Delete</SwipeAction>
-    </TrailingActions>
-  );
-
-  // if (localStorage.getItem.length === true) {
-  //   localStorage.getItem(todoList);
-  // } else {
-  //   setTodoList(INIT_TODO);
-  // }
-
-  // const swiped = false;
-
-  // const deleteListItem = (e) => {
-  //   const { dataset } = e.currentTarget;
-  //   const { id } = dataset;
-  //   if (swiped) {
-  //     const filteredList = [...todoList.filter(id)];
-  //     localStorage.removeItem(todoList.id);
-  //     setTodoList(filteredList);
-  //   }
-  // };
+  const [isDrag, setIsDrag] = useState(false);
+  const [locationX, setLocationX] = useState(null);
+  const [taskWidth, setTaskWidth] = useState(null);
+  const [dragTarget, setDragTarget] = useState(null);
 
   const handleChange = (e) => {
-    const { dataset, checked } = e.currentTarget;
-    const { id } = dataset;
-
+    const { id } = e.currentTarget.dataset;
     setTodoList((prev) => {
       const targetIndex = prev.findIndex((todo) => todo.id === Number(id));
-      const newList = [...prev];
-      newList[targetIndex].done = checked;
+      const newList = todoList.map((todo) => ({ ...todo }));
+      newList[targetIndex].done = !newList[targetIndex].done;
       return newList;
     });
   };
+
+  const handleMouseDown = (e) => {
+    setLocationX(e.pageX);
+    const { id } = e.currentTarget.dataset;
+    setDragTarget(id);
+    setTaskWidth(e.currentTarget.getBoundingClientRect().width);
+    setIsDrag(true);
+  };
+  const handleMouseUp = () => {
+    setIsDrag(false);
+  };
+  const handleMouseMove = (e) => {
+    const { id } = e.currentTarget.dataset;
+    const mouseMovePositionX = e.pageX;
+    const dragDistance = locationX - mouseMovePositionX;
+    if (isDrag && id === dragTarget && dragDistance >= 80) {
+      e.currentTarget.style.transform = `translateX(-${taskWidth / 2}px)`;
+    }
+  };
+
+  const changeSlideStyle = (taskNode, taskList) => {
+    taskNode.style.transform = `translateX(${taskWidth / 4}px)`;
+    taskNode.style.opacity = '0';
+    taskNode.style.height = '0';
+    taskNode.style.margin = '0';
+
+    setTimeout(() => {
+      setTodoList(taskList);
+    }, 1200);
+  };
+
+  const handleDeleteTodo = (e) => {
+    e.preventDefault();
+    const { id } = e.currentTarget.dataset;
+    const listNode = e.currentTarget.parentNode.parentNode.parentNode;
+    const targetIndex = todoList.findIndex((todo) => todo.id === Number(id));
+    const deleteAfterList = todoList.map((todo) => ({ ...todo }));
+    deleteAfterList.splice(targetIndex, 1);
+
+    changeSlideStyle(listNode, deleteAfterList);
+  };
+
+  const handleUndoTodo = (e) => {
+    e.preventDefault();
+    e.target.offsetParent.style.transform = 'translateX(0)';
+  };
+
+  const list = todoList.map((todo) => (
+    <li key={`todo-${todo.id}`} className={styles.taskBox}>
+      <div
+        className={styles.task}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        aria-hidden='true'
+        data-id={todo.id}
+      >
+        <div data-id={todo.id} className={styles.taskStatic}>
+          <div className={styles.checkboxWrapper}>
+            <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={handleChange} />
+            <CheckIcon />
+          </div>
+          <p className={todo.done ? `${styles.title} ${styles.clear}` : styles.title}>{todo.title}</p>
+        </div>
+
+        <div className={styles.taskDelete} data-id={todo.id}>
+          <button type='button' data-id={todo.id} className={styles.deleteIcon} onClick={handleDeleteTodo}>
+            <img src={DeleteIcon} alt='delete_icon' />
+          </button>
+          <p className={styles.deleteText}>The task was deleted</p>
+          <button type='button' data-id={todo.id} className={styles.deleteButton} onClick={handleUndoTodo}>
+            UNDO
+          </button>
+        </div>
+      </div>
+    </li>
+  ));
 
   return (
     <>
       <h1>Hi! this is your assignment.</h1>
       <p className={styles.tasksTitle}>Today&apos;s</p>
-      <ul className={styles.tasks}>
-        {todoList.map((todo) => (
-          <SwipeableList Tag='li' key={`todo-${todo.id}`} className={styles.task}>
-            <SwipeableListItem trailingActions={trailingActions()}>
-              <div className={styles.checkboxWrapper}>
-                <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={handleChange} />
-                <CheckIcon />
-              </div>
-              <p className={styles.title}>{todo.title}</p>
-            </SwipeableListItem>
-          </SwipeableList>
-        ))}
-      </ul>
-      <Swiper />
+      <ul className={styles.taskContainer}>{list}</ul>
     </>
   );
 }
