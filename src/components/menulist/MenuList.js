59d9e294c1bd270153e8dc1c6271c5cb2fed3c22
Jaehyuk <jaehyukj3@gmail.com>
2022-05-06 15:54:18 +0900

    react-icons 추가 menulist에 아이콘 추가

--- src/components/menulist/MenuList.js
+++ src/components/menulist/MenuList.js
@@ -1,19 +1,20 @@
 import styles from '../../routes/Menu/Menu.module.scss';
+import { BiBookmark , BiGridAlt, BiTime, BiCog } from "react-icons/bi";
 
 function MenuList() {
   return (
     <ul className={styles.menulist}>
       <li key="tamplates" className={styles.userMenu}>
-        <p className={styles.menusTitle}>Tamplates</p>
+        <p className={styles.menusTitle}><BiBookmark className={styles.menuIcon} />Tamplates</p>
       </li>
       <li key="categories" className={styles.userMenu}>
-        <p className={styles.menusTitle}>Categories</p>
+        <p className={styles.menusTitle}><BiGridAlt className={styles.menuIcon} />Categories</p>
       </li>
       <li key="analystics" className={styles.userMenu}>
-        <p className={styles.menusTitle}>Analytics</p>
+        <p className={styles.menusTitle}><BiTime className={styles.menuIcon} />Analytics</p>
       </li>
       <li key="settings" className={styles.userMenu}>
-        <p className={styles.menusTitle}>Settings</p>
+        <p className={styles.menusTitle}><BiCog className={styles.menuIcon} />Settings</p>
       </li>
     </ul>
   );
